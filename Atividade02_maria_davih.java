64331e3f58a70d429189d65b8096bf74379face4
Davi Herick <daviherick8@gmail.com>
2025-09-17 19:47:10 -0300

    Add files via upload

---
+++ Atividade02_maria_davih.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade02_maria_davih;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade02_maria_davih {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
