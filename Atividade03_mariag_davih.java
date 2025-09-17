2c92c217f2f041e1796dd79cfc822c5f8d46a3ec
Davi Herick <daviherick8@gmail.com>
2025-09-17 19:47:25 -0300

    Add files via upload

---
+++ Atividade03_mariag_davih.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03_mariag_davih;
+
+/**
+ *
+ * @author Aluno
+ */
+
+import java.util.Scanner;
+
+public class Atividade03_mariag_davih {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double e,s;
+        
+    }
+    
+}
