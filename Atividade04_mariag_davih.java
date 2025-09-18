206203ac3852a6b84bdd09a1f9c25f3f906dbfae
Davi Herick <daviherick8@gmail.com>
2025-09-18 19:12:51 -0300

    Add files via upload

---
+++ Atividade04_mariag_davih.java
@@ -0,0 +1,215 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04_mariag_davih;
+
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04_mariag_davih {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        
+      //1
+//        double a;
+//        System.out.print("Insira um numero: ");
+//        a = input.nextDouble();
+//        if(a >0){
+//            System.out.print("O numero é positivo");
+//        }else{
+//            System.out.print("O numero é negativo");
+//        }
+//        
+      //2
+//        double a;
+//        System.out.print("Insira um numero: ");
+//        a = input.nextDouble();
+//        if(a >0){
+//            System.out.print("O numero é positivo");
+//        }if(a< 0){
+//            System.out.print("O numero é negativo");
+//        }else{
+//            System.out.print("O numero é 0");
+//        }
+//    //3
+//        double a,b,c,d;
+//        System.out.print("Insira 1° numero: ");
+//        a = input.nextDouble();
+//        System.out.print("Insira 2° numero: ");
+//        b = input.nextDouble();
+//        System.out.print("Insira 3° numero: ");
+//        c = input.nextDouble();
+//        
+//        d = (a + b + c) /3;
+//        if(d >=7){
+//            System.out.print("Aluno aprovado.");
+//        }else{
+//            System.out.print("Aluno reprovado.");
+//        }
+      //4
+//        double a,b,c,d;
+//        System.out.print("Insira 1° numero: ");
+//        a = input.nextDouble();
+//        System.out.print("Insira 2° numero: ");
+//        b = input.nextDouble();
+//        System.out.print("Insira 3° numero: ");
+//        c = input.nextDouble();
+//        
+//        d = (a + b + c) /3;
+//        System.out.print(d);
+//        if(d >=7){
+//            System.out.print("Aluno aprovado.");
+//        }if(d>=5 && d<7){
+//            System.out.print("Aluno de recuperação.");
+//        }else{
+//            System.out.print("Aluno reprovado.");
+//        }
+
+      //5
+//        double a;
+//        System.out.print("Insira um numero: ");
+//        a = input.nextDouble();
+//        if(a %2==0){
+//            System.out.print("O numero é par");
+//        }else{
+//            System.out.print("O numero é impar");
+//        }
+      //6
+    //    double a,b,c;
+    //    System.out.print("Digite o 1º valor: ");
+    //    b = input.nextDouble();
+    //    System.out.print("Digite o 2º valor: ");
+    //    a = input.nextDouble();
+    //    System.out.print("Digite o 3º valor: ");
+    //    c = input.nextDouble();
+    //      
+    //      System.out.print(Math.max(a,Math.max(b,c)));
+//      
+        //7
+//        int i;
+//        System.out.print("Insira sua idade: ");
+//        i = input.nextInt();
+//        
+//        if (i>=18 ){
+//            System.out.print("Adulto");}
+//        if (i>= 14 && i<18){
+//            System.out.print("Juvenil");}
+//        if(i>=9 && i <14){
+//            System.out.print("Infantil");}
+//        if (i<9){
+//            System.out.print("Mirim");}
+//        
+         //8
+//        double v1,v2,s;
+//        System.out.print("Insira o 1º valor: ");
+//        v1 = input.nextDouble();
+//        System.out.print("Insira o 2º valor: ");
+//        v2 = input.nextDouble();
+//          
+//        s = v1 + v2;
+//          
+//        if(s>20){
+//          System.out.print(s+8);}
+//        if (s<=20){
+//          System.out.print(s-5);}
+
+        //9
+//          double v;
+//          System.out.print("Insira o valor da compra: ");
+//          v = input.nextDouble();
+//          
+//          if(v<20){
+//              System.out.print(v+(v*0.45));
+//          }
+//          else{System.out.print(v+(v*0.3));}
+//          
+        //10
+//            int a;
+//            System.out.print("Insira a quantidade: ");
+//            a = input.nextInt();
+//            
+//            if(a<12){
+//                System.out.print(a*0.3);
+//            }
+//           else{
+//                System.out.print(a*0.25);
+//            }
+
+        //11
+//            int s;
+//            double a;
+//            System.out.print("Insira sua altura: ");
+//            a = input.nextDouble();
+//            System.out.print("Insira seu sexo (1- Feminino, 2 - Masculino): ");
+//            s = input.nextInt();
+//            
+//            switch (s){
+//                case 1 -> System.out.print((62.1*a)-44.7);
+//                case 2 -> System.out.print((72.7*a)-58);
+//            }
+
+        // 12
+//              int d;
+//              System.out.print("Insira o dia da semana: ");
+//              d = input.nextInt();
+//              
+//              switch(d){
+//                  case 1 -> System.out.print("Final de semana");
+//                  case 7 -> System.out.print("Final de semana");
+//                  case 2 -> System.out.print("Util");
+//                  case 3 -> System.out.print("Util");
+//                  case 4 -> System.out.print("Util");
+//                  case 5 -> System.out.print("Util");
+//                  case 6 -> System.out.print("Util");
+//                  default -> System.out.print("Invalido");
+//              }
+
+        //13
+//                int qtd, cod;
+//                System.out.print("Insira o codigo do produto ");
+//                cod = input.nextInt();
+//                System.out.print("Insira a quantidade do produtoo: ");
+//                qtd = input.nextInt();
+//                
+//                switch(cod){
+//                    case 100 -> System.out.print("Valor à pagar: R$" + 1.59*qtd);
+//                    case 101 -> System.out.print("Valor a pagar: R$ " + 4.59*qtd);
+//                    case 102 -> System.out.print("Valor a pagar: R$" + 1.79*qtd);
+//                    default -> System.out.print("Codigo invalido");
+//                }
+//                
+
+        //14
+//                double v1,v2;
+//                int op;
+//                
+//                System.out.print("Insira o operador: ");
+//                op = input.nextInt();
+//                System.out.print("Insira o 1º valor: ");
+//                v1 = input.nextDouble();
+//                System.out.print("Insira o 2º valor: ");
+//                v2 = input.nextDouble();
+//                
+//                switch(op){
+//                  case 1 -> System.out.print(v1+v2);
+//                  case 7 -> System.out.print(v1-v2);
+//                  case 2 -> System.out.print(v1*v2);
+//                  case 3 -> System.out.print(v1/v2);
+//                  default -> System.out.print("Operador invalido");
+//                }
+                
+                
+                
+             
+    }
+    
+}
