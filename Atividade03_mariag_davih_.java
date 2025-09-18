1701e47ec1991accb2903ab7b9906eba0ce7c32c
Davi Herick <daviherick8@gmail.com>
2025-09-18 19:14:14 -0300

    Add files via upload

---
+++ Atividade03_mariag_davih_.java
@@ -0,0 +1,180 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03_marig_davih_;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03_mariag_davih_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       
+//          double E , S;;
+//          System.out.println("Insira a quantidade de porções: ");
+//          E = input.nextDouble();
+//          S = (200*E) /8;
+//          System.out.println(S);
+
+
+        //2
+
+
+//          int M, E;
+//          System.out.println("Insira o número de impressora: ");
+//          E = input.nextInt();
+//          M = (E * 12) / 3;
+//          System.out.println(M);
+
+        //3
+//          double L, K;
+//          System.out.println("disância: ");
+//          K = input.nextDouble();
+//          L = (18 * K)/ 240;
+//          System.out.println(L);
+
+        // 4
+//          int M, P, H;
+//          System.out.println("Maquina: ");
+//          M = input.nextInt();
+//          System.out.println("Horas: ");
+//          H = input.nextInt();
+//          P = (M * H * 900) / (6 * 5);
+//          System.out.println(P);
+
+        //5
+//        double c, u, valor;
+//        System.out.println("Cotação do real: ");
+//        c = input.nextDouble();
+//        System.out.println("Dolares: ");
+//        u = input.nextDouble();
+//        valor = u*c/1;
+//        System.out.println(valor);
+        
+        //6
+//        int a, b;
+//        double x;
+//        System.out.print("a: ");
+//        a = input.nextInt();
+//        System.out.print("b: ");
+//        b = input.nextInt();
+//        x = -b/a;
+//        System.out.println(a+ "x + " +b+ " = 0 \n x = " +x);
+        
+        //7
+//        double m, b,x,y;
+//        System.out.print("Insira M: ");
+//        m = input.nextDouble();
+//        System.out.print("Insira B: ");
+//        b = input.nextDouble();
+//        System.out.print("Insira X: ");
+//        x = input.nextDouble();
+//        y = m * x + b;
+//        System.out.print(y);
+
+        //8
+//        double x1,x2,y1,y2,m,b,y;
+//        System.out.print("x1: ");
+//        x1 = input.nextDouble();
+//        System.out.print("x2: ");
+//        x2 = input.nextDouble();
+//        System.out.print("y1: ");
+//        y1 = input.nextDouble();
+//        System.out.print("y2: ");
+//        y2 = input.nextDouble();
+//        m = (y2 - y1) / (x2 - x1);
+//        b = y1 - m * x1;
+//        y = m * x1 + b;
+//        System.out.println("y = "+m+" * "+x1+" + "+b);
+//        System.out.print(y);
+                
+        //9
+//         double m1,b1,m2,b2,x,y;
+//         System.out.print("M1: ");
+//         m1 = input.nextDouble();
+//         System.out.print("M2: ");
+//         m2 = input.nextDouble();
+//         System.out.print("B1: ");
+//         b1 = input.nextDouble();
+//         System.out.print("B2: ");
+//         b2 = input.nextDouble();
+//          
+//         x = (b2 - b1)/(m1-m2);
+//         y = m1 * x + b1;
+//         System.out.println(x+","+y);
+
+        //10
+//         int a, b, c;
+//         double  x1 , x2,d;
+//         System.out.print("Insira A: ");
+//         a = input.nextInt();
+//         System.out.print("Insira B: ");
+//         b = input.nextInt();
+//         System.out.print("Insira C: ");
+//         c = input.nextInt();
+//         d = Math.pow(b,2) - 4 * a * c;
+//         x1 = (- b + Math.sqrt(Math.pow(b,2) - 4 * a * c))/(2*a);
+//         x2 = (- b - Math.sqrt(Math.pow(b,2) - 4 * a * c))/(2*a);
+//         System.out.print(d+","+x1+","+x2);
+//            
+//            
+        //11
+        
+//         double d,a,x;
+//         System.out.print("Delta: ");
+//         d = input.nextDouble();
+//         System.out.print("A: ");
+//         a = input.nextDouble();
+//         
+//         x = Math.abs((Math.sqrt(d))/a);
+//         System.out.print(x);
+        
+        //12
+//         double a,b,c,x,f1,f2;
+//         System.out.print("A");
+//         a = input.nextDouble();
+//         System.out.print("B");
+//         b = input.nextDouble();
+//         System.out.print("C");
+//         c = input.nextDouble();
+//         System.out.print("X");
+//         x = input.nextDouble();
+//         f1 = a*(Math.pow(x,2))+b*x+c;
+//         f2 = 2* a * x + b;
+//         System.out.print(f1+","+f2);
+        
+        //13
+//         double m,b,b1,b2,m1,m2,a;
+//         System.out.print("M: ");
+//         m =  input.nextDouble();
+//         System.out.print("B: ");
+//         b =  input.nextDouble();
+//         System.out.print("B1: ");
+//         b1 =  input.nextDouble();
+//         System.out.print("B2: ");
+//         b2 =  input.nextDouble();
+//         m1 = m * b1 + b;
+//         m2 = m * b2 + b;
+//         a = ((m1 + m2)/2)*(b1-b2);
+//         System.out.print(a);
+
+          
+          
+                  
+                  
+    
+    
+    
+    
+    
+    
+    }
+    
+}
