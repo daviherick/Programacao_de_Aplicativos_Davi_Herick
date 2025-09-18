02f557ab294077e9c112b6a7e2c6e3ed618be9d0
Davi Herick <daviherick8@gmail.com>
2025-09-18 19:13:53 -0300

    Add files via upload

---
+++ Atividade02_maria_davih.java
@@ -0,0 +1,213 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade02_mariag;
+
+/**
+ *
+ * @author Aluno
+ */ 
+import java.util.Scanner;
+import java.lang.Math;
+public class Atividade02_mariag {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        /**Scanner input = new Scanner(System.in);
+        String nome;
+        System.out.print("Digite seu nome:");
+        nome = input.nextLine();
+
+        /** nextDuble()
+         * nextBoolen()
+         * nexChar() 
+         * nextInt();
+         */
+        // Questão 1
+        Scanner input = new Scanner(System.in);
+        /*double raio,area,circu;
+        
+        System.out.print("Digite o valor do raio do circulo:");
+        raio = input.nextDouble();
+        
+        area = Math.PI*(Math.pow(raio, 2));
+        circu = 2*Math.PI*raio;
+        
+        System.out.println("Resultado area: "+area);
+        System.out.println("Resulta circunferencia: "+circu);
+        */
+        
+        //Questão 2
+        
+       /*double peso, altura, imc;
+        System.out.println("Digite seu peso emkg: Exemplo 80.0: " );;
+        peso = input.nextDouble();
+        
+        System.out.println("Digite sua altura em metros: Exemplo 1.65: ");
+        altura = input.nextDouble();
+        
+        imc = peso/(altura*altura);
+        System.out.println(imc);
+        */
+        
+        
+        //Questão 3
+        /*
+        double cat1, cat2, hip;
+        System.out.println(" Digite o 1° cateto");
+        cat1 = input.nextDouble();
+        System.out.println(" Digite o 2° cateto");
+        cat2 = input.nextDouble();
+        hip = Math.sqrt(Math.pow(cat1,2)+Math.pow(cat2,2));
+        System.out.println(hip);
+        */
+        
+        //Questão 4
+//        double real,dolar,troco, cal, inn;
+//        System.out.println("Insira o valor em real:");
+//        real = input.nextDouble();
+//        System.out.println("Insira o valor do dolar:");
+//        dolar = input.nextDouble();
+//        cal = real / dolar;
+//        inn = Math.floor(cal);
+//        System.out.println("Dolár: "+cal);
+//        troco = (cal - inn)* dolar;
+//        System.out.println("Troco: "+troco);
+//        
+        
+       // Questão 5
+       /*int n, j1,j2,soma;
+       n = input.nextInt();
+       j1 = input.nextInt();
+       j2 = input.nextInt();
+       soma = (j1+j2) / n
+       System.out.print("Media: " + soma);
+       System.out.println(Math.max(j1,j2))
+       System.out.println(Math.min(j1,j2))
+       */
+       
+       //Questão 6
+       /*double angulo, con, sen, tang;
+       
+       System.out.print("Insira o valor do angulo: ");
+       angulo = input.nextDouble();
+       
+       double graus = Math.toRadians(angulo);
+       con = Math.cos(graus);
+       sen = Math.sin(graus);
+       tang = Math.tan(graus);
+       
+       System.out.println("Cosseno: " +con+ "\nTangente: " +tang+ "\nSeno: " +sen);
+       */
+       
+       //Questão 7
+       /* double cord1x, cord1y, cord2x, cord2y, dis;
+       System.out.print("Insira o valor do X da 1º cordenada: ");
+       cord1x = input.nextDouble();
+       System.out.print("Insira o valor de Y da 1º cordenada: ");
+       cord1y = input.nextDouble();
+       
+       System.out.print("Insira o valor do X da 2º cordenada: ");
+       cord2x = input.nextDouble();
+       System.out.print("Insira o valor de Y da 2º cordenada: ");
+       cord2y = input.nextDouble();
+       
+       dis = Math.sqrt(Math.pow((cord1x - cord2x), 2) + Math.pow((cord1y - cord2y), 2));
+       System.out.println("Distancia euclidiana: "+dis);
+       */
+       
+       //Questão 8
+//       double v1, v2, v3, media, abs, dif;
+//       System.out.print("Insira o 1° valor: ");
+//       v1 = input.nextDouble();
+//       System.out.print("Insira o 2° valor: ");
+//       v2 = input.nextDouble();
+//       System.out.print("Insira o 3° valor: ");
+//       v3 = input.nextDouble();
+//
+//       media = (v1 + v2 + v3) / 3;
+//       abs = Math.abs(v1) + Math.abs(v2) +  Math.abs(v3);
+//       dif = Math.abs(v1 - v2);
+//       System.out.println(media);
+//       System.out.println(abs);
+//       System.out.println(dif);
+//            
+      
+       // Questâo 9
+//        int a, b, c ;
+//        double  x1 , x2;
+//        System.out.print("Insira A: ");
+//        a = input.nextInt();
+//        System.out.print("Insira B: ");
+//        b = input.nextInt();
+//        System.out.print("Insira C: ");
+//        c = input.nextInt();
+//        
+//        x1 = (- b + Math.sqrt(Math.pow(b,2) - 4 * a * c))/(2*a);
+//        x2 = (- b - Math.sqrt(Math.pow(b,2) - 4 * a * c))/(2*a);
+//        System.out.println(x1+","+x2);
+//        
+
+
+        //Questão 10
+          double c , f;
+          System.out.println("Insira a temperatura em fahrenheit: ");
+          f = input.nextDouble();
+          c =  (f - 32) * 5/9;
+          System.out.println(c);
+          System.out.println(f);
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        
+        
+        
+    }
+    
+}
