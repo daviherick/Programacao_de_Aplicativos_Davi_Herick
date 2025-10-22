db062d8ca0363cd0e7cc8a723d56050a18bae7b7
Davi Herick <daviherick8@gmail.com>
2025-10-22 15:21:59 -0300

    Add files via upload

---
+++ Atividade10.sql
@@ -0,0 +1,9 @@
+create database atividade10;
+use atividade10;
+
+create table aluno(
+id int primary key auto_increment,
+nome varchar(100) not null);
+
+
+
---
+++ Atividade10_davih.java
@@ -0,0 +1,11 @@
+
+package atividade10_mariag;
+
+
+public class Atividade10_mariag {
+
+    public static void main(String[] args) {
+     
+    }
+    
+}
